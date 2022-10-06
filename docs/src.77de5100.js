a1ea0c477379e3604775019ab50b79e629d0d717
DanilAnt <denka-123@mail.ru>
2022-10-06 17:59:48 +0300

    fix paths

--- docs/src.77de5100.js
+++ docs/src.77de5100.js
@@ -778,7 +778,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55204" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64572" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
